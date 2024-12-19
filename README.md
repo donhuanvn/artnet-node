72637ba98e601564aff42009b298f31106842797
nhuan <donhuanvn96@gmail.com>
2024-12-19 23:12:28 +0700

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# ESP32 ArtNet Node - Full Master Wireless
---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <esp_log.h>
+
+static const char *TAG = "ArtNet";
+
+extern "C" void app_main(void)
+{
+    ESP_LOGI(TAG, "NHUAN");    
+}
