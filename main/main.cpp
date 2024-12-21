203da55f595c73071e4fd09977eaf1801beb096d
nhuan <donhuanvn96@gmail.com>
2024-12-21 10:07:11 +0700

    Create FreeRTOS task

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <esp_log.h>
+#include "freertos/FreeRTOS.h"
 
 static const char *TAG = "ArtNet";
 
+static void hello_task(void *pvParameters)
+{
+    ESP_LOGI(TAG, "Hello Task starts");
+    while(true)
+    {
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Sleep 3 seconds");
+    }
+}
+
 extern "C" void app_main(void)
 {
-    ESP_LOGI(TAG, "NHUAN");    
+    ESP_LOGI(TAG, "ArtNet Node - Full Master Wireless");
+    xTaskCreate(hello_task, "hello_task", 4096, NULL, configMAX_PRIORITIES - 1, NULL);
 }
